d68d7a3ac0e0cd4930227fe3c4f9f50ef08245ad
ngavinsir <ngavinsir@gmail.com>
2020-08-25 03:11:35 +0700

    add landing page

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<div class="flex flex-col items-center justify-center w-full min-h-screen pb-32">
+    <img
+        class="rounded-full max-w-full"
+        style="height: 192px; width: 192px" 
+        src="https://avatars2.githubusercontent.com/u/29197509?s=460&u=c3519a628d8c6bb6cfac3ad902514da8ca2e69f7&v=4"
+        alt
+    />
+    <h1 class="font-inter text-5xl text-grayscale-9 font-bold mt-8 leading-none">Nathanael Gavin Siregar</h1>
+    <h2 class="font-inter text-3xl text-grayscale-6 font-normal leading-snug">Full-stack software engineer</h2>
+    <div class="flex font-inter text-primary-4 font-semibold text-lg">
+        <a rel="prefetch" href="/about" class="hover:underline px-8 py-12">ABOUT</a>
+        <a rel="prefetch" href="/blog" class="hover:underline px-8 py-12">BLOG</a>
+        <a rel="prefetch" href="/project" class="hover:underline px-8 py-12">PROJECT</a>
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
+const customColor = {
+	grayscale: {
+		1: '#EBEBEB' ,
+		2: '#D6D6D6',
+		3: '#C2C2C2',
+		4: '#ADADAD',
+		5: '#999999',
+		6: '#858585',
+		7: '#707070',
+		8: '#5C5C5C',
+		9: '#474747'
+	},
+	primary: {
+		1: '#DCEEFF' ,
+		2: '#B9DEFF',
+		3: '#95CDFE',
+		4: '#72BDFE',
+		5: '#4FACFE',
+		6: '#3F8ACB',
+		7: '#2F6798',
+		8: '#204566',
+		9: '#102233'
+	}
+}
+
 module.exports = {
 	purge: false,
 	theme: {
-		extend: {},
+		extend: {
+			colors: customColor
+		},
+		fontFamily: {
+			'inter': 'Inter, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+		}
 	},
 	variants: {},
 	plugins: [],
